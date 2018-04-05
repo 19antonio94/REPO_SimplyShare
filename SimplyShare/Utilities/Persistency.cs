d01dc1babb04f17302a6412f2d16c0f10d7dc0ca
Ramalaca94 <giangi_tutolo@yahoo.com>
2018-04-05 12:09:17 +0200

    Aggiunta possibilità impostazione cartella per scaricare file. Ora dati utente in JSON

--- SimplyShare/Utilities/Persistency.cs
+++ SimplyShare/Utilities/Persistency.cs
@@ -5,8 +5,7 @@ using IWshRuntimeLibrary;
 using Microsoft.Win32;
 using SimplyShare.Models;
 using shared;
-
-
+using Newtonsoft.Json;
 
 namespace SimplyShare.Utilities
 {
@@ -20,6 +19,14 @@ namespace SimplyShare.Utilities
             return Path.Combine(currentPath, "User Profile");
         }
 
+        public static string getDownloadDirectory()
+        {
+            //Ricavo la directory corrente
+            string currentPath = Path.GetDirectoryName(System.Reflection.Assembly.GetEntryAssembly().Location);
+            //Genero il path della cartella in cui voglio salvare i dati
+            return Path.Combine(currentPath, "Download");
+        }
+
         public static void install()
         {
             string newPath = getUserDirectory();
@@ -27,7 +34,7 @@ namespace SimplyShare.Utilities
             //Questo if è come se fosse l'installazione automatica al primo avvio
             if (!Directory.Exists(newPath))
             {
-                //Se non esiste creo la directory
+                //Se non esiste creo la directory per l'utente e per i download
                 Directory.CreateDirectory(newPath);
 
                 //TASTO DESTRO
@@ -51,15 +58,29 @@ namespace SimplyShare.Utilities
                 //shortcut...
                 shortcut.Save();
             }
+
+            //Controllo se esiste la cartella di default per i download altrimenti la creo
+            string pathDownload = getDownloadDirectory();
+
+            if (!Directory.Exists(pathDownload))
+                Directory.CreateDirectory(pathDownload);
         }
 
-        public static void saveUserData(Models.LoggedUser user)
+        public static void saveUserData(LoggedUser user)
         {
             string path = getUserDirectory();
-            //Salvo nome e cognome utente in un file .txt
-            System.IO.File.WriteAllText(path + @"\nomeUtente.txt", user.Nome + Environment.NewLine + user.Cognome);
+
+            //Salvo nome, cognome e cartella download in un file .json
+            JsonSerializer serializer = new JsonSerializer();
+            JsonUser objToSerialize = new JsonUser(user);
+            using (StreamWriter sw = new StreamWriter(path + @"\UserData.json"))
+            using (JsonWriter writer = new JsonTextWriter(sw))
+            {
+                serializer.Serialize(writer, objToSerialize);
+            }
+
             //Salvo foto profilo (l'if va tolto se si inserisce una fotoProfilo di default, magari in install)
-            if(user.ProfilePic != null)
+            if (user.ProfilePic != null)
             {
                 JpegBitmapEncoder encoder = new JpegBitmapEncoder();
                 String photolocation = path + @"\fotoUtente.jpg";  //file name 
@@ -73,13 +94,16 @@ namespace SimplyShare.Utilities
         public static LoggedUser loadUserData()
         {
             string path = getUserDirectory();
-            string nome, cognome;
-            
-            if (System.IO.File.Exists(path + @"\nomeUtente.txt"))
+            JsonUser objDeserialized= new JsonUser();
+
+            if (System.IO.File.Exists(path + @"\UserData.json"))
             {
-                var lines = System.IO.File.ReadAllLines(path + @"\nomeUtente.txt");
-                nome = lines[0];
-                cognome = lines[1];
+                using (StreamReader file = System.IO.File.OpenText(path + @"\UserData.json"))
+                {
+                    JsonSerializer serializer = new JsonSerializer();
+                    objDeserialized = (JsonUser)serializer.Deserialize(file, typeof(JsonUser));
+                }
+
                 BitmapImage pic = null;
 
                 if (System.IO.File.Exists(path + @"\fotoUtente.jpg"))
@@ -93,10 +117,10 @@ namespace SimplyShare.Utilities
                     pic.EndInit();
                 }
 
-                return new LoggedUser(pic, nome, cognome, true);
+                return new LoggedUser(pic, objDeserialized.Nome, objDeserialized.Cognome, true, objDeserialized.PathDownload);
             }
 
-            //Ritorna null se non esiste il file nomeUtente.txt
+            //Ritorna null se non esiste il file UserData.json
             return null;
         }
     }
