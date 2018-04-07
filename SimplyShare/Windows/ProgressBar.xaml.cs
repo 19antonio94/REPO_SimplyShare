cefc96874a839fdb7e727a2d48b00afa7eebf127
Ramalaca94 <giangi_tutolo@yahoo.com>
2018-04-07 13:31:27 +0200

    Inizio barra avanzamento

---
+++ SimplyShare/Windows/ProgressBar.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SimplyShare.Windows
+{
+    /// <summary>
+    /// Logica di interazione per ProgressBar.xaml
+    /// </summary>
+    public partial class ProgressBar : Window
+    {
+        public ProgressBar()
+        {
+            InitializeComponent();
+        }
+
+        private void AnnullaButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
